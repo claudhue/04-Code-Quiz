bb9040df79c532335c27b3448c33b17f9f536d68
Francisco Goya <degoyafran@gmail.com>
2020-03-18 21:23:50 -0500

    add timer and setup quiz questions

--- script.js
+++ script.js
@@ -1,67 +1,134 @@
-const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtons = document.getElementById('answer-buttons')
-const buttonBtn = document.getElementsByClassName('btn')
-const nextButton = document.getElementById('next-btn')
-const timerDisplay = document.getElementById('timerDisplay');
-const scoreDiv = document.getElementById('scoreContainer');
+var questionContainerElement = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
+var answerButtonsElement = document.getElementById("answer-buttons");
 
-startButton.addEventListener('click', startQuiz)
-
-
+//Start Quiz
 function startQuiz() {
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
+  var startScreen = document.getElementById("start-screen");
+  if (startScreen.style.display === "none") {
+    startScreen.style.display = "block";
+  } else {
+    startScreen.style.display = "none";
+  }
 }
 
-var score = 0;
-// Create the questions array
+function quizQuestions() {
+  var quizScreen = document.getElementById("quiz-screen");
+  if (quizScreen.style.display === "none") {
+    quizScreen.style.display = "block";
+  } else {
+    quizScreen.style.display = "none";
+  }
+  setNextQuestion();
+}
+
+//Time counter for quiz.
+var timeCounter = document.querySelector(".time");
+
+//Seconds for timer.
+var secondsLeft = 60;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeCounter.textContent = secondsLeft + " seconds left";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      timeout();
+    }
+  }, 1000);
+}
+
+setTime();
+//When timer reaches 0.
+function timeout() {
+  var timeOutHTML = "<h1><center>Results</center></h1>";
+  var element = document.getElementById("quiz-screen");
+  element.innerHTML = timeOutHTML;
+}
+
+//Questions
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answer.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(currentQuestionIndex);
+}
+
+function resetState() {
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  Array.from(answerButtonsElement.children);
+}
+
 var questions = [
-['What is my favorite color?']
-['What is my prefered bevarage choice?'],
-['What is my favorite way of eating avocados?'],
-['Which of the following is NOT a past job?'],
-['Which of the following is my dream travel destination?']
+  {
+    question: "1. Scott Tells Ramona That Pac-Man Was Originally Called ?",
+    answer: [
+      { text: "Dot-Man", correct: false },
+      { text: "Muck-Man", correct: false },
+      { text: "Get-Man", correct: false },
+      { text: "Puck-Man", correct: true }
+    ]
+  },
+  {
+    question: "2. Scott’s ‘Zero’ T-Shirt Is A Reference To A Song By ?",
+    answer: [
+      { text: "The Smashing Pumpkins", correct: true },
+      { text: "Radiohead", correct: false },
+      { text: "Sex Bob-Omb", correct: false },
+      { text: "The Cranberries", correct: false }
+    ]
+  },
+  {
+    question: "3. When Did Scott Date Kim ?",
+    answer: [
+      { text: "They never dated", correct: false },
+      { text:  "College", correct: false },
+      { text:  "Highschool", correct: true },
+      { text: "Elementary School", correct: false }
+    ]
+  },
+  {
+    question: "4. What Tea Does Ramona Choose For Herself And Scott ?",
+    answer: [
+      { text: "Blueberry", correct: false },
+      { text: "Raspberry", correct: false },
+      { text: "Ginseng", correct: false },
+      { text: "Sleepy Time", correct: true }
+    ]
+  },
+  {
+    question: "5. Who Is The Lead Singer Of The Band <i>The Clash at Demonhead<i> ?",
+    answer: [
+      { text: "Knives Chau", correct: false },
+      { text: "Envy Adams",correct: true },
+      { text: "Gideon",  correct: false },
+      { text: "Todd Ingram", correct: false }
+    ]
+  }
 ];
 
-var answers = [
-    ['green','blue','red','black']
-    ['water','tequila','beer','soda']
-    ['toast', 'guac','baked','all']
-    ['optician','receptionist','fast food','home improvement']
-    ['Vietnam','Portugal','Spain','Canada']
-]
+var nextButton = document.getElementById("next-btn");
+var currentQuestionIndex = questions[0];
 
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-
-function timer(){
-    var sec = 30;
-    var timer = setInterval(function(){
-        document.getElementById('TimerDisplay').innerHTML='00:'+sec;
-        sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
-    }, 1000);
-}
-timer.addEventListener('click', startQuiz)
-
-
-
-
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-// Caclulate score
-  function scoreTest(answer, questions) {
-  var score = (answer/questions) * 100;
-  return score;
-  }
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
